40789a3b48368c4372e66b447fc912446cb6b28e
Oluseyi Idowu <oluseyi.idowu@gtbank.com>
2021-02-20 19:14:10 +0100

    initial-commit

---
+++ dist/precache-manifest.c708441f44ff80f208e7fb6b70e3c6db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10b045f9c4c3032e70a",
+    "url": "/css/app.99a2b5a2.css"
+  },
+  {
+    "revision": "cf259dfd254686a87341cf8c02587778",
+    "url": "/fonts/PantonDemo-Black.cf259dfd.otf"
+  },
+  {
+    "revision": "d09549c1ab4a5947a007561521e45da3",
+    "url": "/fonts/SFProDisplay-Regular.d09549c1.ttf"
+  },
+  {
+    "revision": "29c59894d985fa3a774072de20b22da0",
+    "url": "/img/Lines.29c59894.png"
+  },
+  {
+    "revision": "0c0746b1e44dea73151315b014d4a0ae",
+    "url": "/img/default_image2.0c0746b1.png"
+  },
+  {
+    "revision": "8a51c69c5b2b8fc4f22d98ca09ddcdad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c10b045f9c4c3032e70a",
+    "url": "/js/app.d156e494.js"
+  },
+  {
+    "revision": "a7a806b76fea77a974c3",
+    "url": "/js/chunk-vendors.a7c7775c.js"
+  },
+  {
+    "revision": "ebb2524255d77b25b3d241ee440bcbe7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
